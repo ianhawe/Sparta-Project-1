3e8f83825333492c453bdea498a83be1bded37f0
Ian <ianhawe24@hotmail.co.uk>
2018-12-02 13:44:27 +0000

    StartPoint / End Point added

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
+let canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+//x y width height
+
+
 //Class File
-//Make class of circle,
-//Make class of square
+
+//Mouse events
 
 //Circle Class
 //=====================================================================================
+
 //=====================================================================================
 
 //Function to define Circle variables
 //=====================================================================================
 //=====================================================================================
 
-//Other Circle Properties
+//Animate Circle Properties
 //=====================================================================================
 //Random Position Setting of Circles
 //Random Colour Theme Setting
@@ -23,8 +29,69 @@
 
 //Other Square Propperties
 //=====================================================================================
+
 //Start Square Position (Random)
+c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+c.fillRect(10, 60, 20, 20);
+//Start Line
+
+c.beginPath();
+c.moveTo(40, 200);
+c.lineTo(40, 0);
+c.strokeStyle = "#00ff00"
+c.stroke();
+
 //End Square Position (Random)
+c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+c.fillRect(275, 60, 20, 20);
+
+//End Position Line
+// c.beginPath();
+// c.moveTo(270, 200);
+// c.lineTo(270, 0);
+// c.strokeStyle = "#00ff00"
+// c.stroke();
+
+//Box Around End
+
+//top
+c.beginPath();
+c.moveTo(271, 55);
+c.lineTo(299, 55);
+c.strokeStyle = "orange"
+c.stroke();
+
+//Right
+c.beginPath();
+c.moveTo(298, 85);
+c.lineTo(298, 55);
+c.strokeStyle = "orange"
+c.stroke();
+
+//Bottom
+c.beginPath();
+c.moveTo(271, 85);
+c.lineTo(299, 85);
+c.strokeStyle = "orange"
+c.stroke();
+
+
+//Left
+c.beginPath();
+c.moveTo(272, 85);
+c.lineTo(272, 55);
+c.strokeStyle = "orange"
+c.stroke();
+
+
+
+
+// c.beginPath();
+// c.moveTo();
+// c.lineTo();
+// c.strokeStyle = "orange";
+// c.stroke();
+
 //Player Control Area
 
 //=====================================================================================
@@ -40,11 +107,6 @@
 
 //Level Selection
 
+// Colour theme change
 
-
-
-
-
-
-
-
+//Run circles and balls
